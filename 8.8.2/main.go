cc5d1448a93b43ee24fe39f8e24fe8437948a36d
Roman W10 <Safronovraff@gmail.com>
2023-01-17 21:46:42 +0300

    8.8.2

--- 8.8.2/main.go
+++ 8.8.2/main.go
@@ -12,29 +12,30 @@ func main() {
 
 	Dodge := Au.NewConstructAuto("Dodge", "Avenger", 600, 300, 180, 291, 320, "cm")
 
-	fmt.Println("Brand", BMW.Brand(), "Model", BMW.Model(), "EnginePower", BMW.EnginePower(), "MaxSpeed", BMW.MaxSpeed(),
-		"Length", BMW.Length().Get(Au.CM), "Height", BMW.Height().Get(Au.CM), "Width", BMW.Width().Get(Au.CM))
+	/*
+		//works
+		fmt.Println("Brand", BMW.Brand(), "Model", BMW.Model(), "EnginePower", BMW.EnginePower(), "MaxSpeed", BMW.MaxSpeed(),
+			"Length", BMW.Length().Get(Au.CM), "Height", BMW.Height().Get(Au.CM), "Width", BMW.Width().Get(Au.CM))
 
-	fmt.Println("Brand", Mercedes.Brand(), "Model", Mercedes.Model(), "EnginePower", Mercedes.EnginePower(), "MaxSpeed",
-		Mercedes.MaxSpeed(), "Length", Mercedes.Length().Get(Au.CM), "Height", Mercedes.Height().Get(Au.CM), "Width", Mercedes.Width().Get(Au.CM))
+		fmt.Println("Brand", Mercedes.Brand(), "Model", Mercedes.Model(), "EnginePower", Mercedes.EnginePower(), "MaxSpeed",
+			Mercedes.MaxSpeed(), "Length", Mercedes.Length().Get(Au.CM), "Height", Mercedes.Height().Get(Au.CM), "Width", Mercedes.Width().Get(Au.CM))
 
-	fmt.Println("Brand", Dodge.Brand(), "Model", Dodge.Model(), "EnginePower", Dodge.EnginePower(), "MaxSpeed",
-		Dodge.MaxSpeed(), "Length", Dodge.Length().Get(Au.Inch), "Height", Dodge.Height().Get(Au.Inch), "Width", Dodge.Width().Get(Au.Inch))
+		fmt.Println("Brand", Dodge.Brand(), "Model", Dodge.Model(), "EnginePower", Dodge.EnginePower(), "MaxSpeed",
+			Dodge.MaxSpeed(), "Length", Dodge.Length().Get(Au.Inch), "Height", Dodge.Height().Get(Au.Inch), "Width", Dodge.Width().Get(Au.Inch))*/
 
-	//printAuto(BMW)
+	printAuto(BMW)
+	printAuto(Mercedes)
+	printAuto(Dodge)
 }
 
-/*func printAuto(aut Au.AllAuto) {
-
-	fmt.Println(aut.Brand(), aut.Model(), aut.EnginePower(), aut.MaxSpeed())
-
+func printAuto(aut *Au.AllAuto) {
+	fmt.Println("Brand", aut.Brand(), "Model", aut.Model(), "EnginePower", aut.EnginePower())
 	switch aut.Brand() {
 	case "Dodge":
-		fmt.Println(aut.Width().Get(Au.Inch), aut.Height().Get(Au.Inch), aut.Length().Get(Au.Inch))
+		fmt.Println("Width", aut.Width().Get(Au.Inch), "Height", aut.Height().Get(Au.Inch), "Length", aut.Length().Get(Au.Inch))
 
 	default:
-		fmt.Println(aut.Width().Get(Au.CM), aut.Height().Get(Au.CM), aut.Length().Get(Au.CM))
+		fmt.Println("Width", aut.Width().Get(Au.CM), "Height", aut.Height().Get(Au.CM), "Length", aut.Length().Get(Au.CM))
 
 	}
 }
-*/
