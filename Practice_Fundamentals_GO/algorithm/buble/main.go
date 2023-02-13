19944e07ce05e526531b377c2e69796a39f7f00f
Roman W10 <Safronovraff@gmail.com>
2023-02-13 15:01:08 +0300

    bubbleSort

---
+++ Practice_Fundamentals_GO/algorithm/buble/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano()) // необходимо для того, чтобы рандом был похож на рандомный
+}
+
+func main() {
+	ar := make([]int, 50)
+	for i := range ar {
+		ar[i] = rand.Intn(200) - 100 // ограничиваем случайное значение от [-100;100]
+	}
+
+	bubbleSort(ar)
+
+	fmt.Println(ar)
+}
+
+func bubbleSort(ar []int) {
+	l := len(ar)
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-i-1; j++ {
+			if ar[j] > ar[j+1] {
+				ar[j], ar[j+1] = ar[j+1], ar[j]
+			}
+		}
+	}
+}
