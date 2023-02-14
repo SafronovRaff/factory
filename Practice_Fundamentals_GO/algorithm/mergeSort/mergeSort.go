8afe1ea38f5bac4c3b9c2c734576eba81ffb05e3
Roman W10 <Safronovraff@gmail.com>
2023-02-14 14:03:50 +0300

    Sort

--- Practice_Fundamentals_GO/algorithm/mergeSort/mergeSort.go
+++ Practice_Fundamentals_GO/algorithm/mergeSort/mergeSort.go
@@ -15,12 +15,36 @@ func main() {
 	for i := range ar {
 		ar[i] = rand.Intn(200) - 100 // ограничиваем случайно значение от [-100;100]
 	}
-
 	ar = mergeSort(ar)
-
 	fmt.Println(ar)
+
 }
 
 func mergeSort(ar []int) []int {
-	return nil
+	n := len(ar)
+	if n < 2 {
+		return ar
+	}
+
+	mid := n / 2
+	left := mergeSort(ar[:mid])
+	right := mergeSort(ar[mid:])
+	return merge(left, right)
+}
+func merge(left, right []int) []int {
+	res := make([]int, 0, len(left)+len(right))
+
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			res = append(res, left[i])
+			i++
+		} else {
+			res = append(res, right[j])
+			j++
+		}
+	}
+	res = append(res, left[i:]...)
+	res = append(res, right[j:]...)
+	return res
 }
---
+++ Practice_Fundamentals_GO/algorithm/quick/quickSort.go
@@ -0,0 +1,41 @@
+package quick
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano()) // необходимо для того, чтобы рандом был похож на рандомный
+}
+
+func main() {
+	ar := make([]int, 50)
+	for i := range ar {
+		ar[i] = rand.Intn(200) - 100 // ограничиваем случайно значение от [-100;100]
+	}
+
+	quickSort(ar)
+
+	fmt.Println(ar)
+}
+
+func quickSort(ar []int) {
+	if len(ar) < 2 {
+		return
+	}
+	left, right := 0, len(ar)-1
+	povorIndex := rand.Int() % len(ar)
+	ar[povorIndex], ar[right] = ar[right], ar[povorIndex]
+	for i := 0; i < len(ar); i++ {
+		if ar[i] < ar[right] {
+			ar[i], ar[left] = ar[left], ar[i]
+			left++
+		}
+	}
+	ar[left], ar[right] = ar[right], ar[left]
+	quickSort(ar[:left])
+	quickSort(ar[left+1:])
+	return
+}
