60054047c399710326122f0ec21d2be0302203d8
Roman W10 <Safronovraff@gmail.com>
2023-02-17 12:03:10 +0300

    BFS

--- sort/undirectedGraph/undirectedGraph.go
+++ sort/undirectedGraph/undirectedGraph.go
@@ -24,14 +24,18 @@ func main() {
 	g.AddEdge(1, 2)
 	g.AddEdge(2, 3)
 	fmt.Println(g) //{map[0:[1] 1:[0 2] 2:[1 3] 3:[2]]}
-
+	fmt.Println(g.BFS(2))
+	g.AddEdge(3, 4)
+	g.AddEdge(4, 5)
+	fmt.Println(g.BFS(4))
+	fmt.Println(g)
 }
 
 // поиск в ширину BFS
 func (g *Graph) BFS(start int) []int {
 	visited := make(map[int]bool) // словарь для отслеживания посещенных вершин
 	queue := make([]int, 0)       // очередь вершин, которые нужно обработать
-	res := make([]int, 0)         // список посещенных
+	res := make([]int, 0)         // список посещенных вершин
 	visited[start] = true         // помечаем начальную как посещенную
 	queue = append(queue, start)  // добавляем начальную вершину в очередь
 
@@ -41,6 +45,13 @@ func (g *Graph) BFS(start int) []int {
 		queue = queue[1:]       // удаляем из очереди
 		res = append(res, node) // добавляем в список посещенных
 
+		//обработка соедей текущей вершины
+		for _, neighbor := range g.nodes[node] {
+			if !visited[neighbor] { // если сосед еще не посещен
+				visited[neighbor] = true        //помечаем как посещенный
+				queue = append(queue, neighbor) //добавляем соседа в очередь
+			}
+		}
 	}
-	return res
+	return res // возвращает список посещенных вершин
 }
